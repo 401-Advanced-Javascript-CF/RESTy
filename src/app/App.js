2c8d0eb650b4d92a7391700c1d24a4380ea26963
BryantDavis1986 <Bryant.Davis@hotmail.com>
2020-11-18 19:26:20 -0800

    incomplete unable to trigger handleChange when bringing stuff in from history

--- src/app/App.js
+++ src/app/App.js
@@ -3,15 +3,23 @@ import Footer from '../footer/footer.js';
 import Header from '../header/header.js';
 import Crud from '../crud/crud.js';
 import Results from '../results/results.js';
+import History from '../history/history.js';
 
 function App() {
   const [list, setList] = useState(false);
-  console.log(list);
+  const [storage, setStorage] = useState(true);
+  const [methods, setMethods] = useState('');
+  const [urls, setUrls] = useState('');
+
+  function getStorage(name){
+    return JSON.parse(localStorage.getItem(name))
+  }
   return (
     <div>
     <Header />
-    <Crud results={setList}/>
+    <Crud results={setList} setStorage={setStorage} methods={methods} setMethods={setMethods} urls={urls} setUrls={setUrls} />
     <Results results={list} />
+    <History storage={getStorage} isStorage={storage} setMethod={setMethods} setUrl={setUrls}/>
     <Footer />
     </div>
   );
